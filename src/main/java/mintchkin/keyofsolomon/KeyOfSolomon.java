ed52e67049d4a88c739910edbfb802a3b7760bd2
mintchkin <mintchkin.dobfner@gmail.com>
2017-05-14 04:10:23 -0400

    Added some mod info

--- src/main/java/mintchkin/keyofsolomon/KeyOfSolomon.java
+++ src/main/java/mintchkin/keyofsolomon/KeyOfSolomon.java
@@ -1,15 +1,15 @@
-package com.example.examplemod;
+package mintchkin.keyofsolomon;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = KeyOfSolomon.MODID, version = KeyOfSolomon.VERSION)
+public class KeyOfSolomon
 {
-    public static final String MODID = "examplemod";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "keyofsolomon";
+    public static final String VERSION = "0.0.1";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
